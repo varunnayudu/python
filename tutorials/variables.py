fb8c66eac9a3ccf52da2d6b23bf70bab9acbcc5f
varunnayudu <varunnayudu@gmail.com>
2024-01-12 19:55:21 +0400

    added new file named variables.py

---
+++ tutorials/variables.py
@@ -0,0 +1,18 @@
+'''
+In python variables are in smaller cases
+and classes start in big cases
+'''
+
+name = "Varun"
+print(type(name)) # return output as <class 'str'>
+
+num=3
+print(type(num)) # return output as <class 'int'>
+
+nfloat = 3.1222
+print(type(nfloat)) # return output as <class 'float'>
+
+ncomplex = 3j
+print(type(ncomplex)) # return output as <class 'complex'>
+
+
